2b3b8f22eec5650e5dcdb6b28ec84b422e244bb1
justin.hobbs <justinhobbs@fastmail.com>
2022-06-16 21:56:08 -0500

    working core deliverables

--- src/components/ListingsContainer.js
+++ src/components/ListingsContainer.js
@@ -1,14 +1,43 @@
-import React from "react";
-// import ListingCard from "./ListingCard";
+import React, { useState, useEffect } from "react";
+import ListingCard from "./ListingCard";
 
-function ListingsContainer() {
-  return (
-    <main>
-      <ul className="cards">
-        {/* use the ListingCard component to display listings */}
-      </ul>
-    </main>
-  );
+function ListingsContainer({ search }) {
+   const [listings, setListings] = useState([]);
+
+   useEffect(() => {
+      fetch("http://localhost:6001/listings")
+         .then((resp) => resp.json())
+         .then((listings) => setListings(listings));
+   }, []);
+
+   function handleDeleteListing(id) {
+      const updatedListingsArray = listings.filter(
+         (listing) => listing.id !== id
+      );
+      setListings(updatedListingsArray);
+   }
+
+   const filteredListings = listings.filter((listing) => {
+      return listing.description.toLowerCase().includes(search.toLowerCase());
+   });
+
+   console.log(filteredListings);
+
+   const listingCards = filteredListings.map((listingObj) => {
+      return (
+         <ListingCard
+            key={listingObj.id}
+            listing={listingObj}
+            onDeleteListing={handleDeleteListing}
+         />
+      );
+   });
+
+   return (
+      <main>
+         <ul className='cards'>{listingCards}</ul>
+      </main>
+   );
 }
 
 export default ListingsContainer;
